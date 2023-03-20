fc3500c5a29cb14101593900a8c7044eba0ece49
Rubyroido <hvanstas@gmail.com>
2023-03-20 19:12:25 +0500

    сделал меню в header

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ const description = document.querySelector('.description');
 const photoOne = description.querySelector('.description__photo_one');
 const photoTwo = description.querySelector('.description__photo_two');
 
+const headerMenu = document.querySelector('.header__menu');
+const menuButton = document.querySelector('.header__menu-button');
+const headerLinks = document.querySelector('.header__links');
+const listOfLinks = headerLinks.cloneNode(true);
+const menuLinks = Array.from(listOfLinks.childNodes);
+
 const servicesList = document.querySelector('.list');
 const servicesArray = Array.from(servicesList.querySelectorAll('.list__item'));
 const classes = [
@@ -28,8 +34,30 @@ servicesArray.forEach(item => {
     item.addEventListener('click', (e) => {
         let serviceItem = item.firstElementChild;
         serviceItem.classList.toggle('list__item-content_active');
-        if(e.target.classList.contains(classes)) {
+        if (e.target.classList.contains(classes)) {
             serviceItem.classList.toggle('list__item-content_active');
         }
     })
 })
+
+// закрытие меню при нажатии на ссылку
+menuLinks.forEach(link => {
+    headerMenu.prepend(link);
+    link.addEventListener('click', () => {
+        headerMenu.classList.remove('header__menu_active');
+        menuButton.classList.remove('header__menu-button_active');
+    })
+})
+
+// открытие и закрытие меню
+menuButton.addEventListener('click', () => {
+    headerMenu.classList.toggle('header__menu_active');
+    menuButton.classList.toggle('header__menu-button_active');
+    if (headerMenu.classList.contains('header__menu_active')) {
+        description.addEventListener('click', () => {
+            headerMenu.classList.remove('header__menu_active');
+            menuButton.classList.remove('header__menu-button_active');
+        })
+    }
+})
+
